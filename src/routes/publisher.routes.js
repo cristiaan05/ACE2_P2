4eef76b58b698a3b898b4dea4a0365d96edb26a0
Cristian Hernández <chtello05@gmail.com>
2023-10-25 21:57:45 -0600

    routes2

--- src/routes/publisher.routes.js
+++ src/routes/publisher.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { leerSensores, publishA, publishB } from "../controllers/pub.js";
+import { historico1, historico2, historico3, leerSensores, publishA, publishB } from "../controllers/pub.js";
 const routerPub = Router();
 
 routerPub.get('/pub', publishB);
 routerPub.get('/pubA', publishA);
 routerPub.post('/actual', leerSensores);
+routerPub.post('/historico3', historico3);
+routerPub.post('/historico2', historico2);
+routerPub.post('/historico1', historico1);
 export default routerPub;
